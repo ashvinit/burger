899a0fffed0683810428076900b65f6c29491aae
Ashvini Thomas <ashvinithomas@gmail.com>
2020-12-10 19:41:01 -0500

    added routes to get burgers and post burgers

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -4,3 +4,31 @@ var router = express.Router();
 
 //Import the model (burger.js) to use its database functions
 var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+    res.sendFile(path.join(_dirname, "public/index.html"));
+});
+
+//create all our routes and set up logic within those routes where required
+router.get("/burgers", function(req, res) {
+    burger.all(function(data) {
+        res.json({ cats: data });
+    });
+});
+
+router.post("/burgers", function(req, res) {
+    burger.create([
+        "burger_name", "devoured"
+    ],
+    [
+        req.body.burger_name, req.body.devoured
+    ], function(result) {
+        //send back the ID of the new quote
+        res.json({ id: result.insertId });
+    });
+});
+
+
+
+//Export routes for server.js to use
+module.exports = router;
