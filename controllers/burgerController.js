444d2410877e1519239e876ee60543dbf76b86d7
Ashvini Thomas <ashvinithomas@gmail.com>
2020-12-10 19:57:26 -0500

    created delete method for route

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -45,8 +45,18 @@ router.put("/burgers/:id", function(req, res) {
     });
 });
 
-
-
+router.delete("/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+    
+    burger.delete(condition, function(result) {
+        if (result.affectedRows == 0) {
+            //if no rows were changed, thent he ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
 
 //Export routes for server.js to use
 module.exports = router;
